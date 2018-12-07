50e5e1ff022fab72e5e0cb26cf2466f79f1fd548
Dave Rolsky <autarch@urth.org>
2018-12-07 11:15:42 -0800

    Fix example code to actually work

--- example_test.go
+++ example_test.go
@@ -10,10 +10,11 @@ func ExampleIs(t *testing.T) {
 	d := detest.New(t)
 	d.Is(
 		[]int{1, 2, 3},
-		d.Array(func(d *detest.D) {
+		d.Slice(func(d *detest.D) {
 			d.Idx(0, d.Equal(1))
 			d.Idx(1, d.Equal(2))
 			d.Idx(2, d.Equal(4))
 		}),
+		"Slice contains expected values",
 	)
 }
