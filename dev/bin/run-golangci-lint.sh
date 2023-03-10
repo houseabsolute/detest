7898c5a9d6054b7dd706637b69b84fd0ce24aa42
Dave Rolsky <autarch@urth.org>
2023-03-10 17:00:00 -0500

    Allow running golangci-lint in parallel

--- dev/bin/run-golangci-lint.sh
+++ dev/bin/run-golangci-lint.sh
@@ -3,17 +3,20 @@
 status=0
 
 exe=$( which golangci-lint )
+
 if [ -z "$exe" ]; then
     exe="./bin/golangci-lint"
 fi
 
 golangci-lint run \
+    --allow-parallel-runners \
     -c ./.golangci-lint-main.yml \
     --skip-dirs pkg/detest/internal/.+
 
 status+=$?
 
 golangci-lint run \
+    --allow-parallel-runners \
     -c ./.golangci-lint-internal.yml \
     ./pkg/detest/internal/...
 
